5358578af08a0cb160069a9556b2aa272274e40b
Alex <Prestge.alex101@gmail.com>
2026-08-26 12:40:19 -0700

    initial design created

---
+++ Portfolio/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return(
+        <>
+            <h1>Portfolio</h1>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ Portfolio/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <>
+        <div className="absolute left-1/6 bottom-1/2">
+            <p className="text text-4xl">Alexander Ruiz</p>
+            <p className="text text-2xl">Projects</p>
+            <p className="text text-2xl">About Me</p>
+            <p className="text text-2xl">Projects</p>
+            <p className="text text-2xl">Contact Me</p>
+        </div>
+        <div id="vertical-line" />
+    </>
+  )
+}
+
+export default Home;

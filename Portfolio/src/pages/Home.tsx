bc8702ef508ec7cbeed8e97ba0d30b7d1d93dc36
Alex <Prestge.alex101@gmail.com>
2026-09-02 21:06:31 -0700

    replaced some positioning with flex

--- Portfolio/src/pages/Home.tsx
+++ Portfolio/src/pages/Home.tsx
@@ -1,26 +1,30 @@
 import firstImage from '../assets/craiyon_133348_image.png'
 import secondImage from '../assets/craiyon_133437_image.png'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import '../Home.css'
+import '../App.css'
 
 function Home() {
   return (
-    <>
-      <div className="background-container"/>
+    <div className="flex h-screen background-container">
+      <div className="flex flex-col justify-center items-start gap-2 z-1 pl-[clamp(1rem,20vw,100px)]">
+        <h1 className="text-8xl [text-shadow:0_0_10px_#00f0ff,0_0_20px_#00f0ff44]">Alexander Ruiz</h1>
+        <h2 className="text-6xl [text-shadow:0_0_10px_#00f0ff,0_0_20px_#00f0ff44]">Web Developer/SWE</h2>
+        
+        <Link to="/view-my-work" className="text text-6xl glitch">View My Work</Link>
+        <Link to="/about" className="text text-6xl glitch">About Me</Link>
+        <Link to="/contact" className="text text-6xl glitch">Contact Me</Link>
 
-      <div className="flex flex-col gap-2 absolute left-[27%] top-[47%] -translate-x-1/2 -translate-y-1/2">
-          <h1 className="text-8xl [text-shadow:0_0_10px_#00f0ff,0_0_20px_#00f0ff44]">Alexander Ruiz</h1>
-          <h2 className="text-6xl [text-shadow:0_0_10px_#00f0ff,0_0_20px_#00f0ff44]">Web Developer/SWE</h2>
-          
-          <Link to="/view-my-work" className="text text-6xl glitch">View My Work</Link>
-          <Link to="/about" className="text text-6xl glitch">About Me</Link>
-          <Link to="/contact" className="text text-6xl glitch">Contact Me</Link>
+        <p className="text text-2xl">Welcome to my portfolio, Choom</p>
       </div>     
 
-      <img src={firstImage} alt="description" className="image1" />
-      <img src={secondImage} alt="description" className="image2" />
-      <div className="half-circle" />
-      <div id="vertical-line" />
-    </>
+      <div className="flex items-center justify-end w-1/2">
+        <img src={firstImage} alt="description" className="image1" />
+        <img src={secondImage} alt="description" className="image2" />
+        <div className="half-circle" />
+        <div id="vertical-line" />
+      </div>
+    </div>
   )
 }
 

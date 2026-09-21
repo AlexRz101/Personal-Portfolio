995435dd9796dc62a8cf77cad115bdc8e9f7cafb
Alex <Prestge.alex101@gmail.com>
2026-09-21 15:38:43 -0700

    Another responsiveness fix

--- Portfolio/src/pages/Contact.tsx
+++ Portfolio/src/pages/Contact.tsx
@@ -28,7 +28,7 @@ function Contact() {
   return (
     <div className="contact-page min-h-screen bg-[#1a1a1a]">
 
-      <div className="responsive-navbar flex justify-center items-start py-8">
+      <div className="flex justify-center items-start py-8">
         <Navbar
           navBarColor="bg-[#f2e900]"
           textColor="text-black"

4f7b1c31b51681cfad9d64223ae7be546fc77a75
Alex <Prestge.alex101@gmail.com>
2026-09-21 15:56:42 -0700

    About page layout done

--- Portfolio/src/pages/About.tsx
+++ Portfolio/src/pages/About.tsx
@@ -22,12 +22,14 @@ function About() {
 
             {/* Page content below */}
             <div className="flex flex-col items-center justify-center text-center h-[70%] max-w-2xl mx-auto text-left">
-                <h1 className="text">About Me</h1>
-                <h2 className="text">Welcome to my about page!</h2>
-                <p className="about-content center">My name is Alexander Ruiz and I am a passionate Web Developer/Software Engineer.
-                    I have experience in building web applications using modern technologies such as React, Spring Boot and PostgreSQL.
-                    I enjoy solving complex problems and creating efficient solutions.
-                    In my free time, I like to explore new technologies and work on personal projects to enhance my skills.</p>
+                <div className="bg-black rounded-[50%] px-30 py-34 flex flex-col items-center gap-3">
+                    <h1 className="text text-[#f2e900]">About Me</h1>
+                    <h2 className="text text-[#f2e900]">Welcome to my about page!</h2>
+                    <p className="about-content text-white">My name is Alexander Ruiz and I am a passionate Web Developer/Software Engineer.
+                        I have experience in building web applications using modern technologies such as React, Spring Boot and PostgreSQL.
+                        I enjoy solving complex problems and creating efficient solutions.
+                        In my free time, I like to explore new technologies and work on personal projects to enhance my skills.</p>
+                </div>
             </div>
 
             <img src={leftImage} alt="description" className="leftImage" />
